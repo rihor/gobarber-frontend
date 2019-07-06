8db61af679b52db26d128ce76187fabd90a32990
Pedro Pinho <rihor1000@gmail.com>
2019-07-06 13:44:43 -0300

    Initial commit

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  presets: [
+    // responsável por alterar as funcionalidades do javascript que o navegador ainda não entende
+    "@babel/preset-env",
+    // responsável por alterar as funcionalidades do react que o navegador não entende, exemplo: jsx e etc...
+    "@babel/preset-react"
+  ]
+};
