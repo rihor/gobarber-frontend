0cad483328a4ebf325c084897217670e33444707
Pedro Pinho <rihor1000@gmail.com>
2019-07-06 14:02:16 -0300

    Configurando importações de css

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'index.js'),
+  entry: path.resolve(__dirname, "src", "index.js"),
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js"
   },
 
   // live reload
   devServer: {
-    contentBase: path.resolve(__dirname, 'public'),
+    contentBase: path.resolve(__dirname, "public")
   },
 
   module: {
@@ -19,8 +19,16 @@ module.exports = {
         test: /.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader"
         }
+      },
+      {
+        test: /.css$/,
+        /**
+         * style-loader carrega os arquivos de css na pagina
+         * css-loader permite importações dentro do css
+         */
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
       }
     ]
   }
